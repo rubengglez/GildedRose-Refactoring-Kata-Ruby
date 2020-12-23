31f68df50ac7e441ccddfb6b09c6fa60ac46dcc9
Rubén García Glez <rubengglez@gmail.com>
2020-12-23 19:43:05 +0100

    max value of quality is 50. Reply action

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -27,9 +27,7 @@ class GildedRose
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = [item.quality + 1, 50].min
             end
           end
         end
