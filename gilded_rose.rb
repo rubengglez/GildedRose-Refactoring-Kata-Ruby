b72c9ee1ad9773e94208552d08e9e0e0c4a2297f
Rubén García Glez <rubengglez@gmail.com>
2020-12-23 19:43:51 +0100

    max value of quality is 50. Reply action

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -23,7 +23,7 @@ class GildedRose
           if item.name == BACKSTAGE
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality = [item.quality + 1, 50].min
               end
             end
             if item.sell_in < 6
